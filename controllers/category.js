6d93f28ec8b45bf866b5cc323c4ad9b0f8fdd8b3
Sergey <43466490+KuzSerg@users.noreply.github.com>
2018-09-26 15:31:56 +0300

    Beautyfull

--- controllers/category.js
+++ controllers/category.js
@@ -1,53 +1,46 @@
-const Category = require('../models/Category')
-const errorHandler = require('../utils/errorHandler')
-const position = require('../models/Position')
-module.exports.getAll = async function(req,res){
-    try {
-        const categories = await Category.find({user: req.user.id})
-        res.status(200).json(categories)
-    }
-    catch (e) {
-        errorHandler(res, e)
-    }
-}
+const Category = require('../models/Category');
+const errorHandler = require('../utils/errorHandler');
+const position = require('../models/Position');
+module.exports.getAll = async function(req, res) {
+	try {
+		const categories = await Category.find({ user: req.user.id });
+		res.status(200).json(categories);
+	} catch (e) {
+		errorHandler(res, e);
+	}
+};
 
-module.exports.getById = async function(req,res){
-    try {
-        const category = await Category.findById(req.params.id)
-        res.status(200).json(category)
-    }
-    catch (e) {
-        errorHandler(res, e)
-    }
-}
+module.exports.getById = async function(req, res) {
+	try {
+		const category = await Category.findById(req.params.id);
+		res.status(200).json(category);
+	} catch (e) {
+		errorHandler(res, e);
+	}
+};
 
-module.exports.remove = async function(req,res){
-    try {
-        await Category.remove({_id: req.params.id})
-        await position.remove({category: req.params.id})
-        res.status(200).json({
-            message: 'Категория удалена'
-        })
-    }
-    catch (e) {
-        errorHandler(res, e)
-    }
-}
+module.exports.remove = async function(req, res) {
+	try {
+		await Category.remove({ _id: req.params.id });
+		await position.remove({ category: req.params.id });
+		res.status(200).json({
+			message: 'Категория удалена',
+		});
+	} catch (e) {
+		errorHandler(res, e);
+	}
+};
 
-module.exports.create = function(req,res){
-    try {
+module.exports.create = function(req, res) {
+	try {
+	} catch (e) {
+		errorHandler(res, e);
+	}
+};
 
-    }
-    catch (e) {
-        errorHandler(res, e)
-    }
-}
-
-module.exports.update = function(req,res){
-    try {
-
-    }
-    catch (e) {
-        errorHandler(res, e)
-    }
-}
+module.exports.update = function(req, res) {
+	try {
+	} catch (e) {
+		errorHandler(res, e);
+	}
+};
--- models/Category.js
+++ models/Category.js
@@ -1,32 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 //Создаем схему описывающую нашу модель
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 //Присваиваем переменной categorySchema отвечающая за схему,
 //сюда передаем объект конфигурации
-const categorySchema = new Schema ({
-    //Создаем новый ключ
-    name: {
-        //У каждого ключа есть тип - указываем его
-        type: String,
-        //Указывает на модификатор - поле обязательное
-        required: true
-    },
-    imageSrc: {
-        //У каждого ключа есть тип - указываем его
-        type: String,
-        //Указывает на модификатор - поле необязательно
-        default: ''
-    },
-    user: {
-        //Создаем ссылку на указывающую коллекцию
-        ref: 'users',
-        //Указываем для коллекции тип - Schema.Types.ObjectId
-        type: Schema.Types.ObjectId
-    }
-
-})
+const categorySchema = new Schema({
+	//Создаем новый ключ
+	name: {
+		//У каждого ключа есть тип - указываем его
+		type: String,
+		//Указывает на модификатор - поле обязательное
+		required: true,
+	},
+	imageSrc: {
+		//У каждого ключа есть тип - указываем его
+		type: String,
+		//Указывает на модификатор - поле необязательно
+		default: '',
+	},
+	user: {
+		//Создаем ссылку на указывающую коллекцию
+		ref: 'users',
+		//Указываем для коллекции тип - Schema.Types.ObjectId
+		type: Schema.Types.ObjectId,
+	},
+});
 
 //Экспорт модели:
 //Создаем нужную нам таблицу/коллекцию и передаем в нее созданную схему
-module.export = mongoose.model('categories', categorySchema)
+module.export = mongoose.model('categories', categorySchema);
